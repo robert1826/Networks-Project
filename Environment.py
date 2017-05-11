c929e9128307423370bddc8b4d11cc435d62861e
alfredsamy <alfred_samy2010@yahoo.com>
2017-05-11 15:56:45 +0200

    fix and improvement

--- Environment.py
+++ Environment.py
@@ -1,8 +1,10 @@
 from Node import *
+import time
 class Environment:
 	Nodes = {}	# map[node_id] => Node
 
 	def send(self, sender_ip, receiver_ip, msg):
+		print(sender_ip,'send',msg['type'],'to',receiver_ip)
 		self.Nodes[receiver_ip].receive(sender_ip, msg)
 
 
@@ -12,34 +14,18 @@ if __name__ == '__main__':
 	# 1-2
 	# |
 	# 3-4
-	n1 = Node(env, 1 , 1)
-	n2 = Node(env, 2 , 2)
-	n3 = Node(env, 3 , 3)
-	n4 = Node(env, 4 , 4)
-	env.Nodes[1] = n1
-	env.Nodes[2] = n2
-	env.Nodes[3] = n3
-	env.Nodes[4] = n4
+	for i in range(1,5):
+		env.Nodes[i] = Node(env, i, i)
 
-	n1.Immediate_Neighbours[2] = [0,0,0]
-	n1.Immediate_Neighbours[3] = [0,0,0]
+	env.Nodes[1].add_neighbour(2)
+	env.Nodes[1].add_neighbour(3)
+	env.Nodes[3].add_neighbour(4)
 
-	n2.Immediate_Neighbours[1] = [0,0,0]
+	time.sleep(1)
 
-	n3.Immediate_Neighbours[1] = [0,0,0]
-	n3.Immediate_Neighbours[4] = [0,0,0]
-
-	n4.Immediate_Neighbours[3] = [0,0,0]
-
-	n1.hello()
-
-	print(n1.GPS_Map)
-	print(n2.GPS_Map)
-	print(n3.GPS_Map)
-	print(n4.GPS_Map)
+	for i in env.Nodes:
+		print(env.Nodes[i].GPS_Map)
 	print()
-	n1.request_gps(4)
-	print(n1.GPS_Map)
-	print(n2.GPS_Map)
-	print(n3.GPS_Map)
-	print(n4.GPS_Map)
+	env.Nodes[2].request_gps(4)
+	for i in env.Nodes:
+		print(env.Nodes[i].GPS_Map)
--- Node.py
+++ Node.py
@@ -5,6 +5,7 @@
 # 	4-dst_ip
 # 	5-src_gps
 from Environment import *
+import time, threading
 class Node:
 	# Environment = None
 	# IP = None # my ip
@@ -12,7 +13,6 @@ class Node:
 	# GPS_Map = {} # map[node_ip] => its gps
 	# Immediate_Neighbours = {} # map[node_ip] => (t1, t2, t3)
 	# sended_msgs = {} # map[node_ip] => list of msg id(id1,id2...) to prevent resend the same msg again
-
 
 	def __init__(self, env, ip, gps):
 		self.Environment = env
@@ -22,10 +22,14 @@ class Node:
 		self.Immediate_Neighbours = {}
 		self.sended_msgs = {}
 		self.GPS_Map = {}
-		self.sended_msgs[ip] = [0]
 
+		self.hello()
+
+	def add_neighbour(self, node):
+		self.Immediate_Neighbours[node] = [0,0,0]
 
 	def hello(self):
+		# print(self.IP,'send hello')
 		for node_ip in self.Immediate_Neighbours:
 			# send(ip, msg)
 			msg = {'type' : 'hello', 'id' : self.msg_id ,
@@ -33,6 +37,7 @@ class Node:
 					'src_gps' : self.GPS_Location}
 			self.msg_id += 1
 			self.Environment.send(self.IP, node_ip, msg)
+		threading.Timer(1, self.hello).start()
 
 	def receive(self, sender_ip, msg):
 		# First save src_gps and send ACK
@@ -41,17 +46,20 @@ class Node:
 			self.send_ACK(sender_ip, msg)
 
 		if msg['type'] == 'hello':
+			if(sender_ip not in self.Immediate_Neighbours):
+				self.Immediate_Neighbours[sender_ip] = [0,0,0]
 			return
 		
-		elif (msg['type'] == 'gps_request' or msg['type'] == 'gps_replay') and msg['dst_ip'] != self.IP:
+		elif (msg['type'] == 'gps_request' or msg['type'] == 'gps_reply') and msg['dst_ip'] != self.IP:
 			# Forward the msg
 			self.send_msg_test(msg)
 
 		elif msg['type'] == 'gps_request':
-			rlp_msg = {'type' : 'gps_reply','id':msg['id'],
+			rlp_msg = {'type' : 'gps_reply','id':self.msg_id,
 						'src_ip' : self.IP, 'dst_ip' : msg['src_ip'],
 						'src_gps' : self.GPS_Location}
-			self.Environment.send(self.IP, msg['src_ip'], rlp_msg)
+			self.msg_id += 1
+			self.send_msg_test(rlp_msg)
 		elif msg['type'] == 'ACK':
 			# Update time for msg[src_ip]
 			return
@@ -76,6 +84,7 @@ class Node:
 		else:
 			self.sended_msgs[msg['src_ip']] = []
 		self.sended_msgs[msg['src_ip']].append(msg['id'])
+
 		for node_ip in self.Immediate_Neighbours:
 			self.Environment.send(self.IP, node_ip, msg)
 
