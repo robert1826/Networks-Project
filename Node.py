aba8d9a5817ed13880947e2780231f77fa58214a
alfredsamy <alfred_samy2010@yahoo.com>
2017-05-11 18:01:43 +0200

    Figure out the region for next hop

--- Node.py
+++ Node.py
@@ -5,7 +5,7 @@
 # 	4-dst_ip
 # 	5-src_gps
 from Environment import *
-import time, threading
+import time, threading, math
 class Node:
 	# Environment = None
 	# IP = None # my ip
@@ -52,14 +52,14 @@ class Node:
 		
 		elif (msg['type'] == 'gps_request' or msg['type'] == 'gps_reply') and msg['dst_ip'] != self.IP:
 			# Forward the msg
-			self.send_msg_test(msg)
+			self.send_msg(msg)
 
 		elif msg['type'] == 'gps_request':
 			rlp_msg = {'type' : 'gps_reply','id':self.msg_id,
 						'src_ip' : self.IP, 'dst_ip' : msg['src_ip'],
 						'src_gps' : self.GPS_Location}
 			self.msg_id += 1
-			self.send_msg_test(rlp_msg)
+			self.send_msg(rlp_msg)
 		elif msg['type'] == 'ACK':
 			# Update time for msg[src_ip]
 			return
@@ -70,9 +70,33 @@ class Node:
 		self.Environment.send(self.IP, node_ip, msg)
 
 	def send_msg(self, msg):
+		if(msg['dst_ip'] not in self.GPS_Map):
+			self.send_msg_test(msg)
+			return
+		# This message was send before or not
+		f = False
+		if msg['src_ip'] in self.sended_msgs:
+			for i in self.sended_msgs[msg['src_ip']]:
+				if(i == msg['id']):
+					return
+		else:
+			self.sended_msgs[msg['src_ip']] = []
+		self.sended_msgs[msg['src_ip']].append(msg['id'])
+
 		# 1- determine possible neighbor
+		possible_nodes = []
+		ang = 0
+		while(len(possible_nodes) == 0):
+			ang += 45
+			for node_ip in self.Immediate_Neighbours:
+				if(self.in_region(self.GPS_Map[msg['dst_ip']], self.GPS_Map[node_ip], ang)):
+					possible_nodes.append(node_ip)
+			print("******************",possible_nodes)
+		
 		# 2- choose one neighbor to send msg to
-		# 3- wait for reply
+		for node_ip in possible_nodes:
+			self.Environment.send(self.IP, node_ip, msg)
+		
 		return 0
 
 	def send_msg_test(self, msg):
@@ -94,3 +118,33 @@ class Node:
 				'src_gps' : self.GPS_Location}
 		self.msg_id += 1
 		self.send_msg_test(msg)
+
+	def in_region(self, dest_gps, gps, ang = 45):
+
+		m = (dest_gps[1] - self.GPS_Location[1]) / (dest_gps[0] - self.GPS_Location[0]-0.0000000000000001)
+		angle = math.atan(m)
+
+		p = self.rotate_point(gps[:], angle)
+		# print(p)
+
+		m = p[1] / (p[0]+0.0000000001)
+		angle = math.degrees(math.atan(m))
+		if(angle<= ang and angle >= -1*ang):
+			return True
+		return False
+
+	def rotate_point(self, p, angle):
+		s = math.sin(angle)
+		c = math.cos(angle)
+
+		# translate point back to origin:
+		p[0] -= self.GPS_Location[0]
+		p[1] -= self.GPS_Location[1]
+
+		# rotate point
+		xnew = p[0] * c + p[1] * s
+		ynew = -1* p[0] * s + p[1] * c
+		
+		# xnew = xnew + cx
+		# ynew = ynew + cy
+		return [xnew,ynew]
